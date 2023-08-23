e893b232bb047d67f512b23b5c85b05e5b6d3779
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2023-08-23 23:02:04 +0530

    (PROD)[TRUE] 🍙 (#17)

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,4 +1,4 @@
-const production: boolean = false;
+const production: boolean = true;
 export const API_URL = (production) ? "https://api.bitspace.org.in" : "http://localhost:6969"
 
 export const CLIENT_URL = (production) ? "https://bitspace.org.in" : "http://localhost:3000"
