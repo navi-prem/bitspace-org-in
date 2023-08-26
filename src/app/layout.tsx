47c2987a4e46fd555e01177ab495ec053682dbe5
Jesvi Jonathan <47633894+jesvijonathan@users.noreply.github.com>
2023-08-26 09:47:51 +0530

    Added Scroll Animation (#26)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-'use client'
+"use client";
 import { Header } from "@/libs/components/layouts/header";
+import "jos-animation/dist/jos.css";
 import "./globals.css";
 import "../libs/assets/fonts/Inter/inter.css";
 import { useUserStore } from "@/libs/stores";
@@ -7,101 +8,119 @@ import Cookies from 'js-cookie';
 import axios from "axios";
 import { API_URL } from "@/libs/constants";
 import { useEffect, useState } from "react";
+import jos from "jos-animation/dist/jos.min.js";
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    const accessToken = Cookies.get("bs_access_token");
+  const accessToken = Cookies.get("bs_access_token");
 
-    const userStore = useUserStore((state) => state.user);
-    const setUserStore = useUserStore((state) => state.setUser);
+  const userStore = useUserStore((state) => state.user);
+  const setUserStore = useUserStore((state) => state.setUser);
 
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            if (accessToken && !userStore) {
-                try {
-                    const { data } = await axios.get(API_URL + "/me", { withCredentials: true });
-                    setUserStore(data);
-                } catch (error) {
-                    console.log(error);
-                } finally {
-                    setLoading(false);
-                }
-            } else {
-                setLoading(false);
-            }
-        };
+  const jos_options = {
+    debugMode: false,
+    passive: true,
+    animation: "fade",
+    duration: 0.4,
+    rootMargin: "20% 0% 30% 0%",
+  };
 
-        fetchUser();
-    }, [accessToken, userStore]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (accessToken && !userStore) {
+        try {
+          const { data } = await axios.get(API_URL + "/me", {
+            withCredentials: true,
+          });
+          setUserStore(data);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => console.log(loading), [loading])
+    fetchUser();
+  }, [accessToken, userStore]);
 
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8" />
-                <meta name="theme-color" content="#000000" />
-                <meta
-                    name="description"
-                    content="Bit Space (@bitspaceorg) : A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
-                />
-                <meta
-                    name="keywords"
-                    content="bitspaceorg, bitspace cit, cit bitspace, open source, :bitspace"
-                />
-                <meta name="author" content="Bit Space" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="theme-color" content="#000000" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@bitspaceorg" />
-                <meta name="twitter:creator" content="@bitspaceorg" />
-                <meta name="twitter:title" content="Bit Space" />
-                <meta
-                    name="twitter:description"
-                    content="A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
-                />
-                <meta
-                    name="twitter:image"
-                    content="https://cdn.discordapp.com/attachments/1106935324556406866/1122462968311992350/metaimg1x1.png"
-                />
-                <meta property="og:title" content="Bit Space" />
-                <meta
-                    property="og:description"
-                    content="A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
-                />
-                <meta
-                    property="og:image"
-                    content="https://cdn.discordapp.com/attachments/1106935324556406866/1122462968311992350/metaimg1x1.png"
-                />
-                <meta property="og:url" content="https://bitspace.org" />
-                <meta property="og:site_name" content="Bit Space" />
-                <meta property="og:type" content="website" />
-                <meta property="og:locale" content="en_US" />
-                <meta property="og:locale:alternate" content="en_US" />
+  useEffect(() => console.log(loading), [loading]);
 
-                <link rel="icon" href="./favicon.ico" />
-            </head>
-            <body className="relative flex flex-col items-stretch bg-white font-inter">
-                {loading ? (
-                    <div className="flex flex-col items-center justify-center w-screen h-screen">
-                        <img className="w-[100px] animate-bounce" src="https://media.discordapp.net/attachments/1106935324556406866/1128657375260311583/logocir.png?width=1242&height=1024" />
-                    </div>
-                ) : (
-                    <>
-                        <Header />
-                        <main className="h-main mt-[8vh]">{children}</main>
-                    </>
-                )
-                }
-            </body>
-        </html>
-    );
+  useEffect(() => {
+    jos.init(jos_options);
+  }, []);
+
+  useEffect(() => {
+    jos.refresh();
+  });
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Bit Space (@bitspaceorg) : A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
+        />
+        <meta
+          name="keywords"
+          content="bitspaceorg, bitspace cit, cit bitspace, open source, :bitspace"
+        />
+        <meta name="author" content="Bit Space" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@bitspaceorg" />
+        <meta name="twitter:creator" content="@bitspaceorg" />
+        <meta name="twitter:title" content="Bit Space" />
+        <meta
+          name="twitter:description"
+          content="A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
+        />
+        <meta
+          name="twitter:image"
+          content="https://cdn.discordapp.com/attachments/1106935324556406866/1122462968311992350/metaimg1x1.png"
+        />
+        <meta property="og:title" content="Bit Space" />
+        <meta
+          property="og:description"
+          content="A community started by students from Chennai Institute of Technology. Aims in building community for awareness and positive impact on open source projects."
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.discordapp.com/attachments/1106935324556406866/1122462968311992350/metaimg1x1.png"
+        />
+        <meta property="og:url" content="https://bitspace.org" />
+        <meta property="og:site_name" content="Bit Space" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:locale:alternate" content="en_US" />
+
+        <link rel="icon" href="./favicon.ico" />
+      </head>
+      <body className="relative flex flex-col items-stretch bg-white font-inter">
+        {loading ? (
+          <div className="flex flex-col items-center justify-center w-screen h-screen">
+            <img
+              className="w-[100px] animate-bounce"
+              src="https://media.discordapp.net/attachments/1106935324556406866/1128657375260311583/logocir.png?width=1242&height=1024"
+            />
+          </div>
+        ) : (
+          <>
+            <Header />
+            <main className="h-main mt-[8vh]">{children}</main>
+          </>
+        )}
+      </body>
+    </html>
+  );
 }
--- src/app/u/me/page.tsx
+++ src/app/u/me/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { API_URL } from "@/libs/constants"
 import axios from "axios"
-import { useEffect, useState, ChangeEvent } from "react"
-import { useUserStore } from "@/libs/stores";
+import { ChangeEventHandler, useEffect, useState, ChangeEvent, FormEventHandler } from "react"
+import {useUserStore} from "@/libs/stores";
 import Image from "next/image"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
@@ -30,14 +30,14 @@ const Me = () => {
         points: 0,
         Role: [],
         init: "",
-    })
+})
     const [show, setShow] = useState(false)
     const [curr, setCurr] = useState<User>(data)
     const [roles, setRoles] = useState<Role[]>([])
     const [current, setCurrent] = useState('')
 
     const call = async () => {
-        await axios.put(`${API_URL}/user`, curr)
+        const { data } = await axios.put(`${API_URL}/user`, curr)
     }
 
     const handleAdd = () => {
@@ -45,7 +45,7 @@ const Me = () => {
         const temp = { role: current, username: data.username }
         let boolean = false
         curr.Role.forEach(element => {
-            if (element.role === current) boolean = true
+            if (element.role === current) boolean = true 
         });
         // ROLE ALREADY EXISTS
         if (boolean) {
@@ -69,7 +69,7 @@ const Me = () => {
         setCurrent('')
     }
 
-    const handle = (e: React.SyntheticEvent) => {
+    const handle =  (e: React.SyntheticEvent) => {
         e.preventDefault()
         let lst: Role[] = []
         curr.Role.forEach(ele => {
@@ -91,7 +91,7 @@ const Me = () => {
     const handleRankChange = (event: ChangeEvent<HTMLInputElement>) => {
         setCurr((e: User) => {
             return {
-                ...e, rank: parseInt(event.target.value)
+                ...e, rank: event.target.value
             }
         })
     }
@@ -131,7 +131,7 @@ const Me = () => {
     const handlePoints = (event: ChangeEvent<HTMLInputElement>) => {
         setCurr((e: User) => {
             return {
-                ...e, points: parseInt(event.target.value)
+                ...e, points: event.target.value
             }
         })
     }
@@ -164,7 +164,7 @@ const Me = () => {
         <div className="flex justify-around">
             <div className="flex flex-col items-center w-full">
                 <br />
-                <Image className="rounded-full" src={`https://github.com/${data.github_id}.png`} alt="Image couldn't be loaded." width='200' height='200' />
+                <Image className="rounded-full" src={`https://github.com/${data.github_id}.png`} alt="Image couldn't be loaded." width='200' height='200'/>
                 <div>
                     <div className="text-[50px] inline font-bold">{data.github_id}</div>&nbsp;&nbsp;
                     <EditIcon onClick={() => setShow(e => !e)} className="cursor-pointer inline mb-4 scale-[1.25]" />
@@ -172,70 +172,70 @@ const Me = () => {
                 <div className="mb-2 font-extrabold text-[#9D8AE8] text-xl">{data.title}</div>
                 <div>
                     <Link className="inline" href={`https://github.com/${data.username}`}>
-                        {(
-                            <div className="inline">
-                                <GitHubIcon className="inline mb-2" />&nbsp;
-                                <div className="inline text-xl font-extrabold underline">{data.username}</div>
-                            </div>
-                        )}
+                    {(
+                        <div className="inline">
+                            <GitHubIcon className="inline mb-2" />&nbsp;
+                            <div className="inline text-xl font-extrabold underline">{data.username}</div>
+                        </div>
+                    )}
                     </Link>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <Link className="inline" href={`https://discordapp.com/users/${data.discord_id}`}>
-                        {(
-                            <div className="inline">
-                                <svg className="inline mb-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">    <path d="M19.952,5.672c-1.904-1.531-4.916-1.79-5.044-1.801c-0.201-0.017-0.392,0.097-0.474,0.281 c-0.006,0.012-0.072,0.163-0.145,0.398c1.259,0.212,2.806,0.64,4.206,1.509c0.224,0.139,0.293,0.434,0.154,0.659 c-0.09,0.146-0.247,0.226-0.407,0.226c-0.086,0-0.173-0.023-0.252-0.072C15.584,5.38,12.578,5.305,12,5.305S8.415,5.38,6.011,6.872 c-0.225,0.14-0.519,0.07-0.659-0.154c-0.14-0.225-0.07-0.519,0.154-0.659c1.4-0.868,2.946-1.297,4.206-1.509 c-0.074-0.236-0.14-0.386-0.145-0.398C9.484,3.968,9.294,3.852,9.092,3.872c-0.127,0.01-3.139,0.269-5.069,1.822 C3.015,6.625,1,12.073,1,16.783c0,0.083,0.022,0.165,0.063,0.237c1.391,2.443,5.185,3.083,6.05,3.111c0.005,0,0.01,0,0.015,0 c0.153,0,0.297-0.073,0.387-0.197l0.875-1.202c-2.359-0.61-3.564-1.645-3.634-1.706c-0.198-0.175-0.217-0.477-0.042-0.675 c0.175-0.198,0.476-0.217,0.674-0.043c0.029,0.026,2.248,1.909,6.612,1.909c4.372,0,6.591-1.891,6.613-1.91 c0.198-0.172,0.5-0.154,0.674,0.045c0.174,0.198,0.155,0.499-0.042,0.673c-0.07,0.062-1.275,1.096-3.634,1.706l0.875,1.202 c0.09,0.124,0.234,0.197,0.387,0.197c0.005,0,0.01,0,0.015,0c0.865-0.027,4.659-0.667,6.05-3.111 C22.978,16.947,23,16.866,23,16.783C23,12.073,20.985,6.625,19.952,5.672z M8.891,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913s1.674,0.857,1.674,1.913S9.816,14.87,8.891,14.87z M15.109,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913c0.924,0,1.674,0.857,1.674,1.913S16.033,14.87,15.109,14.87z" /></svg>
-                                &nbsp;
-                                <div className="inline text-xl font-extrabold underline">{data.discord_id}</div>
-                            </div>
-                        )}
+                    {(
+                        <div className="inline">
+                            <svg className="inline mb-1" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M19.952,5.672c-1.904-1.531-4.916-1.79-5.044-1.801c-0.201-0.017-0.392,0.097-0.474,0.281 c-0.006,0.012-0.072,0.163-0.145,0.398c1.259,0.212,2.806,0.64,4.206,1.509c0.224,0.139,0.293,0.434,0.154,0.659 c-0.09,0.146-0.247,0.226-0.407,0.226c-0.086,0-0.173-0.023-0.252-0.072C15.584,5.38,12.578,5.305,12,5.305S8.415,5.38,6.011,6.872 c-0.225,0.14-0.519,0.07-0.659-0.154c-0.14-0.225-0.07-0.519,0.154-0.659c1.4-0.868,2.946-1.297,4.206-1.509 c-0.074-0.236-0.14-0.386-0.145-0.398C9.484,3.968,9.294,3.852,9.092,3.872c-0.127,0.01-3.139,0.269-5.069,1.822 C3.015,6.625,1,12.073,1,16.783c0,0.083,0.022,0.165,0.063,0.237c1.391,2.443,5.185,3.083,6.05,3.111c0.005,0,0.01,0,0.015,0 c0.153,0,0.297-0.073,0.387-0.197l0.875-1.202c-2.359-0.61-3.564-1.645-3.634-1.706c-0.198-0.175-0.217-0.477-0.042-0.675 c0.175-0.198,0.476-0.217,0.674-0.043c0.029,0.026,2.248,1.909,6.612,1.909c4.372,0,6.591-1.891,6.613-1.91 c0.198-0.172,0.5-0.154,0.674,0.045c0.174,0.198,0.155,0.499-0.042,0.673c-0.07,0.062-1.275,1.096-3.634,1.706l0.875,1.202 c0.09,0.124,0.234,0.197,0.387,0.197c0.005,0,0.01,0,0.015,0c0.865-0.027,4.659-0.667,6.05-3.111 C22.978,16.947,23,16.866,23,16.783C23,12.073,20.985,6.625,19.952,5.672z M8.891,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913s1.674,0.857,1.674,1.913S9.816,14.87,8.891,14.87z M15.109,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913c0.924,0,1.674,0.857,1.674,1.913S16.033,14.87,15.109,14.87z"/></svg>
+                            &nbsp;
+                            <div className="inline text-xl font-extrabold underline">{data.discord_id}</div>
+                        </div>
+                    )}
                     </Link>
                 </div>
                 <div>
-                    <LeaderboardIcon className="inline mb-2" />&nbsp;
+                    <LeaderboardIcon className="inline mb-2"/>&nbsp;
                     <div className="inline text-xl font-extrabold">Leaderboard points: {data.points} pts</div>
                 </div>
                 <div className="text-xl font-extrabold">Hierarchy Rank: {data.rank}</div>
             </div>
-            {show && (
-                <div className="h-[86vh] w-[60%] rounded-xl border-4 border-black p-2 m-4">
+            { show && (
+                <div className="h-[86vh] w-[60%] rounded-xl border border-4 border-black p-2 m-4">
                     <form onSubmit={handle} className="bg-[#9D8AE8] min-w-full h-full rounded-xl border border-3 border-black p-10">
                         <div className="w-full">
                             <div className="flex items-center justify-between">
-                                <div className="font-extrabold text-[20px]">Username:</div> <input className="w-[60%] text-box" value={curr.github_id} onChange={handleNameChange} type="text" />
+                                <div className="font-extrabold text-[20px]">Username:</div> <input className="w-[60%] text-box" value={curr.github_id} onChange={handleNameChange} type="text"/>
                             </div>
                             <div className="flex items-center justify-between">
-                                <div className="font-extrabold text-[20px]">Title:</div> <input className="w-[60%] text-box" value={curr.title} onChange={handleTitleChange} type="text" />
+                                <div className="font-extrabold text-[20px]">Title:</div> <input className="w-[60%] text-box" value={curr.title} onChange={handleTitleChange} type="text"/>
                             </div>
                         </div>
                         <div className="flex justify-around w-full">
                             <GitHubIcon className="inline mt-6" />
-                            <input className="inline text-box w-[100%]" value={curr.username} onChange={handleGithubChange} type="text" /><br />
-                            <svg className="inline mt-6 scale-[2.5] m-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">    <path d="M19.952,5.672c-1.904-1.531-4.916-1.79-5.044-1.801c-0.201-0.017-0.392,0.097-0.474,0.281 c-0.006,0.012-0.072,0.163-0.145,0.398c1.259,0.212,2.806,0.64,4.206,1.509c0.224,0.139,0.293,0.434,0.154,0.659 c-0.09,0.146-0.247,0.226-0.407,0.226c-0.086,0-0.173-0.023-0.252-0.072C15.584,5.38,12.578,5.305,12,5.305S8.415,5.38,6.011,6.872 c-0.225,0.14-0.519,0.07-0.659-0.154c-0.14-0.225-0.07-0.519,0.154-0.659c1.4-0.868,2.946-1.297,4.206-1.509 c-0.074-0.236-0.14-0.386-0.145-0.398C9.484,3.968,9.294,3.852,9.092,3.872c-0.127,0.01-3.139,0.269-5.069,1.822 C3.015,6.625,1,12.073,1,16.783c0,0.083,0.022,0.165,0.063,0.237c1.391,2.443,5.185,3.083,6.05,3.111c0.005,0,0.01,0,0.015,0 c0.153,0,0.297-0.073,0.387-0.197l0.875-1.202c-2.359-0.61-3.564-1.645-3.634-1.706c-0.198-0.175-0.217-0.477-0.042-0.675 c0.175-0.198,0.476-0.217,0.674-0.043c0.029,0.026,2.248,1.909,6.612,1.909c4.372,0,6.591-1.891,6.613-1.91 c0.198-0.172,0.5-0.154,0.674,0.045c0.174,0.198,0.155,0.499-0.042,0.673c-0.07,0.062-1.275,1.096-3.634,1.706l0.875,1.202 c0.09,0.124,0.234,0.197,0.387,0.197c0.005,0,0.01,0,0.015,0c0.865-0.027,4.659-0.667,6.05-3.111 C22.978,16.947,23,16.866,23,16.783C23,12.073,20.985,6.625,19.952,5.672z M8.891,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913s1.674,0.857,1.674,1.913S9.816,14.87,8.891,14.87z M15.109,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913c0.924,0,1.674,0.857,1.674,1.913S16.033,14.87,15.109,14.87z" /></svg>
-                            <input className='inline text-box w-[100%]' value={curr.discord_id} onChange={handleDiscordChange} type="number" /><br />
+                            <input className="inline text-box w-[100%]" value={curr.username} onChange={handleGithubChange} type="text"/><br />
+                            <svg className="inline mt-6 scale-[2.5] m-2" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M19.952,5.672c-1.904-1.531-4.916-1.79-5.044-1.801c-0.201-0.017-0.392,0.097-0.474,0.281 c-0.006,0.012-0.072,0.163-0.145,0.398c1.259,0.212,2.806,0.64,4.206,1.509c0.224,0.139,0.293,0.434,0.154,0.659 c-0.09,0.146-0.247,0.226-0.407,0.226c-0.086,0-0.173-0.023-0.252-0.072C15.584,5.38,12.578,5.305,12,5.305S8.415,5.38,6.011,6.872 c-0.225,0.14-0.519,0.07-0.659-0.154c-0.14-0.225-0.07-0.519,0.154-0.659c1.4-0.868,2.946-1.297,4.206-1.509 c-0.074-0.236-0.14-0.386-0.145-0.398C9.484,3.968,9.294,3.852,9.092,3.872c-0.127,0.01-3.139,0.269-5.069,1.822 C3.015,6.625,1,12.073,1,16.783c0,0.083,0.022,0.165,0.063,0.237c1.391,2.443,5.185,3.083,6.05,3.111c0.005,0,0.01,0,0.015,0 c0.153,0,0.297-0.073,0.387-0.197l0.875-1.202c-2.359-0.61-3.564-1.645-3.634-1.706c-0.198-0.175-0.217-0.477-0.042-0.675 c0.175-0.198,0.476-0.217,0.674-0.043c0.029,0.026,2.248,1.909,6.612,1.909c4.372,0,6.591-1.891,6.613-1.91 c0.198-0.172,0.5-0.154,0.674,0.045c0.174,0.198,0.155,0.499-0.042,0.673c-0.07,0.062-1.275,1.096-3.634,1.706l0.875,1.202 c0.09,0.124,0.234,0.197,0.387,0.197c0.005,0,0.01,0,0.015,0c0.865-0.027,4.659-0.667,6.05-3.111 C22.978,16.947,23,16.866,23,16.783C23,12.073,20.985,6.625,19.952,5.672z M8.891,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913s1.674,0.857,1.674,1.913S9.816,14.87,8.891,14.87z M15.109,14.87c-0.924,0-1.674-0.857-1.674-1.913 s0.749-1.913,1.674-1.913c0.924,0,1.674,0.857,1.674,1.913S16.033,14.87,15.109,14.87z"/></svg>
+                            <input className='inline text-box w-[100%]' value={curr.discord_id} onChange={handleDiscordChange} type="number"/><br />
                         </div>
                         <div className="w-full">
                             <div className="flex items-center justify-between">
-                                <div className="font-extrabold text-[20px]">Leaderboard pts:</div> <input className="text-box w-[30%]" value={curr.points} onChange={handlePoints} type="number" />
+                                <div className="font-extrabold text-[20px]">Leaderboard pts:</div> <input className="text-box w-[30%]" value={curr.points} onChange={handlePoints} type="number"/>
                             </div>
                             <div className="flex items-center justify-between">
-                                <div className="font-extrabold text-[20px]">Rank:</div> <input className=" text-box w-[30%]" value={curr.rank} onChange={handleRankChange} type="number" />
+                                <div className="font-extrabold text-[20px]">Rank:</div> <input className=" text-box w-[30%]" value={curr.rank} onChange={handleRankChange} type="number"/>
                             </div>
                         </div>
                         <div>
-                            <div className="flex items-center justify-between w-full">
-                                <div className="font-extrabold text-[20px]">Roles:</div>
-                                <div className="w-full">
-                                    <input className="w-[60%] text-box" value={current} onChange={(e) => setCurrent(e.target.value)} />
-                                    <AddSharpIcon onClick={handleAdd} className="font-extrabold scale-[1.5] m-1" />
-                                </div>
+                        <div className="flex items-center justify-between w-full">
+                            <div className="font-extrabold text-[20px]">Roles:</div>
+                            <div className="w-full">
+                                <input className="w-[60%] text-box" value={current} onChange={(e) => setCurrent(e.target.value)}/>
+                                <AddSharpIcon onClick={handleAdd} className="font-extrabold scale-[1.5] m-1" />
                             </div>
-                            <div className="pt-3 pb-3 bg-white border-4 border-black rounded">
-                                {curr.Role.map((ele: any, i: any) => {
-                                    return (
-                                        <div className="p-2 border-2 border-black rounded-md w-[125px] font-bold inline m-1" key={i}>{ele.role.substr(0, 9) + ' '}{(<CancelOutlinedIcon onClick={(e) => handleClose(i)} />)}</div>
-                                    )
-                                })}
-                            </div>
+                        </div>
+                        <div className="pt-3 pb-3 bg-white border border-4 border-black rounded">
+                            {curr.Role.map((ele: any, i: any) => {
+                                return (
+                                    <div className="p-2 border border-2 border-black rounded-md w-[125px] font-bold inline m-1" key={i}>{ele.role.substr(0, 9)+' '}{(<CancelOutlinedIcon onClick={(e) => handleClose(i)} />)}</div>
+                                )
+                            })}
+                        </div>
                             <button type='submit' className="px-2 py-1 my-3 text-white bg-black rounded-xl">Edit</button>
                         </div>
                     </form>
